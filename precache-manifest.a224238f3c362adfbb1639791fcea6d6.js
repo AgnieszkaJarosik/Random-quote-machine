4e95348862a71af9d7525f5a5fbb8d58006ec683
Aga <agajarosik@o2.pl>
2019-09-20 20:11:30 +0200

    Updates

---
+++ precache-manifest.a224238f3c362adfbb1639791fcea6d6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f65151f0fa55a503a03ccbeece9883",
+    "url": "/Random-quote-machine/index.html"
+  },
+  {
+    "revision": "c46636df91b6d2de5ca0",
+    "url": "/Random-quote-machine/static/css/main.557228f7.chunk.css"
+  },
+  {
+    "revision": "0957d392d8cb438ca060",
+    "url": "/Random-quote-machine/static/js/2.53b75ad8.chunk.js"
+  },
+  {
+    "revision": "c46636df91b6d2de5ca0",
+    "url": "/Random-quote-machine/static/js/main.c77aa02c.chunk.js"
+  },
+  {
+    "revision": "2628f3237eef58303b73",
+    "url": "/Random-quote-machine/static/js/runtime~main.82247b03.js"
+  },
+  {
+    "revision": "40fadace06065957871567ad3bc4f9df",
+    "url": "/Random-quote-machine/static/media/fontello.40fadace.svg"
+  },
+  {
+    "revision": "502e79fbb08028c61e21d5ee698ceb10",
+    "url": "/Random-quote-machine/static/media/fontello.502e79fb.woff"
+  },
+  {
+    "revision": "630ce9b087b315d7de165b129acf0c98",
+    "url": "/Random-quote-machine/static/media/fontello.630ce9b0.eot"
+  },
+  {
+    "revision": "67c2de079307f0a1f70e4453f608f82b",
+    "url": "/Random-quote-machine/static/media/fontello.67c2de07.woff2"
+  },
+  {
+    "revision": "dae39ad30b566d46627467768d428755",
+    "url": "/Random-quote-machine/static/media/fontello.dae39ad3.ttf"
+  }
+]);
